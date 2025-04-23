08dbe0d73ee2f38784d6758995c9d3b99b6d74be
ANKURJOSHI1991 <ankurjoshi1991@gmail.com>
2025-04-23 12:54:05 +0530

    admin

--- src/constants/API_URL.jsx
+++ src/constants/API_URL.jsx
@@ -1,3 +1,4 @@
-const API_URL = "http://localhost:3000/api/v1" ;
+// const API_URL = "http://localhost:3000/api/v1" ;
+const API_URL = "https://hotelprojectankur.onrender.com/api/v1" ;
 
 export {API_URL} ;
